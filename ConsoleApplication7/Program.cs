f67340d6ce15e75fb714b6061299dd1e74423e58
Aluno <aluno@naverecife.local>
2019-04-12 09:45:12 -0300

    N2 Oberdan

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        class Empregados
+        {
+            //1 QUESTAO:
+            public string nome;
+            public string sobrenome;
+            public int salario;
+
+            public void AtribuirNome(string trocarNome)
+            {
+                nome = trocarNome;
+
+            }
+            public void AtribuirSobrenome(string trocarSobrenome)
+            {
+                sobrenome = trocarSobrenome;
+            }
+            public void AtribuirSalario(int trocarSalario)
+            {
+                salario = trocarSalario;
+            }
+            //2 QUESTAO
+            public string RetornarNome()
+            {
+                return nome;
+            }
+            public string RetornarSobrenome()
+            {
+                return sobrenome;
+            }
+            public int RetornarSalario()
+            {
+                return salario;
+            }
+            public int Retornar10Porcento()
+            {
+                int Salariox10 = salario * 10;
+                int dezPorcento = Salariox10 / 100;
+                return dezPorcento;
+            }
+
+        }
+    }
+}
